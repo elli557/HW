423af1f860c037e125cf1e5678b0af409ffa4380
emwauqz <emwauqz@gmail.com>
2025-05-08 01:50:01 +0600

    HW

--- first_home_work/views.py
+++ first_home_work/views.py
@@ -4,8 +4,8 @@ def first_page_view(request):
     if request.method == 'GET':
         
         context = {
-            'emoji': '🪶',
-            'text': 'Моё первое домашнее задание на Django',
-            'run_string': 'HomeWork'
+            'emoji': '🎞️',
+            'text': 'Здесь собраны лучшие фильмы, которые я рекомендую всем.',
+            'run_string': 'Каждый фильм - это отдельная история, готовая покорить твое сердце.'
         }
     return render(request, template_name='index.html', context=context)
